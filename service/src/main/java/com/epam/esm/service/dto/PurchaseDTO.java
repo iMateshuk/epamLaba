577844469db1f422ad9a472ce51cb5ab30699953
Ivan Mateshuk <7620536@mail.ru>
2021-12-23 12:10:20 +0300

    Small fix

--- service/src/main/java/com/epam/esm/service/dto/PurchaseDTO.java
+++ service/src/main/java/com/epam/esm/service/dto/PurchaseDTO.java
@@ -8,19 +8,20 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PurchaseDTO {
-  @NotBlank(message = "app.NotBlank.message")
+
+  @NotNull(message = "app.NotNull.message")
   @Min(value = 1, message = "app.Min.message")
   @Max(value = Integer.MAX_VALUE,  message = "app.Max.message")
   private Integer userId;
 
-  @NotBlank(message = "app.NotBlank.message")
+  @NotNull(message = "app.NotNull.message")
   @Min(value = 1, message = "app.Min.message")
   @Max(value = Integer.MAX_VALUE,  message = "app.Max.message")
   private Integer certId;
