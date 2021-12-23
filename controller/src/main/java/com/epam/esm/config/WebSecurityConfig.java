a720d4da12cf513c6d748c1cd8d98157db568b65
Ivan Mateshuk <7620536@mail.ru>
2021-12-23 16:55:10 +0300

    HttpStatus.UNAUTHORIZED

--- controller/src/main/java/com/epam/esm/config/WebSecurityConfig.java
+++ controller/src/main/java/com/epam/esm/config/WebSecurityConfig.java
@@ -5,11 +5,13 @@ import com.epam.esm.filter.JwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
@@ -45,6 +47,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.GET, "/**").hasRole("USER")
         .antMatchers(HttpMethod.POST, "/orders").hasRole("USER")
         .antMatchers("/**").hasRole("ADMIN")
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
     ;
   }
 }
